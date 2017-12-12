e36fee7d2440534daeb2a52c12065518f349d3de
BibiAyesha <BibiAyesha@users.noreply.github.com>
2017-12-12 18:04:02 +0530

    Update calculationsTest.py

--- calculationsTest.py
+++ calculationsTest.py
@@ -1,14 +1,14 @@
 import unittest
-
+from calculations import add, mul, concat 
 class claculationTests(unittest.TestCase):
-  function testAdd(self):
-    self.assertEqual(11 == add(5,6))
+  def testAdd(self):
+    self.assertEqual(11, add(5,6))
   
-  function testMul(self):
-    self.assertEqual(30 == mul(5,6))
+  def testMul(self):
+    self.assertEqual(30, mul(5,6))
  
-  function testConcat(self):
-    self.assertEqual("Hello Ayesha" == concat("Hello ","Ayesha"))
+  def testConcat(self):
+    self.assertEqual("Hello Ayesha", concat("Hello ","Ayesha"))
 
 if __name__ == '__main__':
   unittest.main()
